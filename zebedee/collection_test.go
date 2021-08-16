b7e0ecead4521ec74c58a5b3eb3d487ad99e33b0
CarlHembrough <carlhuk@gmail.com>
2021-08-16 16:10:16 +0100

    Add test coverage for delete collection function

--- zebedee/collection_test.go
+++ zebedee/collection_test.go
@@ -19,18 +19,10 @@ const (
 
 func Test_CreateCollection(t *testing.T) {
 
-	collectionJson := `{
+	responseBody := `{
 		"name": "Coronavirus key indicators"
 	}`
-
-	httpClient := &mock.HttpClientMock{
-		DoFunc: func(ctx context.Context, req *http.Request) (*http.Response, error) {
-			recorder := httptest.NewRecorder()
-			recorder.Code = http.StatusOK
-			recorder.Body = bytes.NewBufferString(collectionJson)
-			return recorder.Result(), nil
-		},
-	}
+	httpClient := mockHttpResponse(http.StatusOK, responseBody)
 	zebedeeClient := NewClient(host, httpClient)
 	session := newSession()
 
@@ -62,20 +54,86 @@ func Test_CreateCollection(t *testing.T) {
 	})
 }
 
-func Test_CreateCollection_RequestError(t *testing.T) {
+func Test_CreateCollection_HttpError(t *testing.T) {
 	expectedError := errors.New("something broke")
-	httpClient := newHttpClientMock()
-	httpClient.DoFunc = func(ctx context.Context, req *http.Request) (*http.Response, error) {
-		return nil, expectedError
-	}
-	zebedeeClient := NewClient(host, httpClient)
 	session := newSession()
+	collectionDescription := NewCollection("Collection Name")
 
-	Convey("Given an error is returned from request object", t, func() {
-		collectionDescription := NewCollection("Collection Name")
+	Convey("Given an error is returned from the HTTP client", t, func() {
+		httpClient := mockHttpError(expectedError)
+		zebedeeClient := NewClient(host, httpClient)
 
 		Convey("When CreateCollection is called", func() {
 			_, err := zebedeeClient.CreateCollection(session, collectionDescription)
+
+			Convey("Then the expected error is returned", func() {
+				So(httpClient.DoCalls(), ShouldHaveLength, 1)
+				So(err, ShouldEqual, expectedError)
+			})
+		})
+	})
+}
+
+func Test_DeleteCollection(t *testing.T) {
+	responseBody := `true`
+	httpClient := mockHttpResponse(http.StatusOK, responseBody)
+	zebedeeClient := NewClient(host, httpClient)
+	session := newSession()
+
+	Convey("Given an ID of a collection to delete", t, func() {
+		collectionId := "1234"
+
+		Convey("When DeleteCollection is called", func() {
+			err := zebedeeClient.DeleteCollection(session, collectionId)
+
+			Convey("Then the expected request is sent to the HTTP client", func() {
+				So(httpClient.DoCalls(), ShouldHaveLength, 1)
+
+				req := httpClient.DoCalls()[0].Req
+				So(req.Method, ShouldEqual, http.MethodDelete)
+				So(req.URL.String(), ShouldEqual, host+"/collection/"+collectionId)
+				So(req.Header.Get(request.FlorenceHeaderKey), ShouldEqual, session.ID)
+				So(req.Header.Get("content-type"), ShouldEqual, "application/json")
+			})
+
+			Convey("Then no error is returned", func() {
+				So(err, ShouldBeNil)
+			})
+		})
+	})
+}
+
+func Test_DeleteCollection_FalseResponse(t *testing.T) {
+	session := newSession()
+	collectionId := "1234"
+
+	Convey("Given an false response body is returned from Zebedee", t, func() {
+		responseBody := `false`
+		httpClient := mockHttpResponse(http.StatusOK, responseBody)
+		zebedeeClient := NewClient(host, httpClient)
+
+		Convey("When DeleteCollection is called", func() {
+			err := zebedeeClient.DeleteCollection(session, collectionId)
+
+			Convey("Then the expected error is returned", func() {
+				So(httpClient.DoCalls(), ShouldHaveLength, 1)
+				So(err.Error(), ShouldEqual, "delete collection request unsuccesseful: 1234")
+			})
+		})
+	})
+}
+
+func Test_DeleteCollection_HttpError(t *testing.T) {
+	expectedError := errors.New("something broke")
+	httpClient := mockHttpError(expectedError)
+	zebedeeClient := NewClient(host, httpClient)
+	session := newSession()
+
+	Convey("Given an ID of a collection to delete", t, func() {
+		collectionId := "1234"
+
+		Convey("When DeleteCollection is called", func() {
+			err := zebedeeClient.DeleteCollection(session, collectionId)
 
 			Convey("Then the expected error is returned", func() {
 				So(httpClient.DoCalls(), ShouldHaveLength, 1)
@@ -92,10 +150,21 @@ func newSession() Session {
 	}
 }
 
-func newHttpClientMock() *mock.HttpClientMock {
+func mockHttpError(err error) *mock.HttpClientMock {
 	return &mock.HttpClientMock{
 		DoFunc: func(ctx context.Context, req *http.Request) (*http.Response, error) {
-			return httptest.NewRecorder().Result(), nil
+			return nil, err
+		},
+	}
+}
+
+func mockHttpResponse(responseCode int, responseBody string) *mock.HttpClientMock {
+	return &mock.HttpClientMock{
+		DoFunc: func(ctx context.Context, req *http.Request) (*http.Response, error) {
+			recorder := httptest.NewRecorder()
+			recorder.Code = responseCode
+			recorder.Body = bytes.NewBufferString(responseBody)
+			return recorder.Result(), nil
 		},
 	}
 }
