0a95c2c7b2a24017b32f72003556224030d03aa9
Dave Llewellyn <439837@gmail.com>
2021-09-30 13:47:08 +0100

    added API error type to hold details about the response body and status

--- zebedee/http.go
+++ zebedee/http.go
@@ -3,21 +3,35 @@ package zebedee
 import (
 	"context"
 	"fmt"
-	dphttp "github.com/ONSdigital/dp-net/http"
 	"io/ioutil"
 	"net/http"
 	"time"
+
+	dphttp "github.com/ONSdigital/dp-net/http"
 )
 
 const (
+	readResponseBodyErrFmt        = "unexpected error attempting to read error response body: %q %s %s expected status: %d actual status: %d"
 	incorrectStatusErrFmt         = "request %s %s expected status %d but received %d"
-	incorrectStatusWithBodyErrFmt = "request %s %s expected status %d but received %d response body: %s"
+	incorrectStatusWithBodyErrFmt = "request %s %s expected status %d but received %d"
 )
 
 //go:generate moq -out mock/httpclient.go -pkg mock . HttpClient
 // HttpClient defines a Zebedee HTTP client
 type HttpClient interface {
 	Do(ctx context.Context, req *http.Request) (*http.Response, error)
+}
+
+// APIError represent an error returned from the Zebedee CMS API.
+type APIError struct {
+	ActualStatus   int
+	ExpectedStatus int
+	Message        string
+	Body           string
+}
+
+func (err *APIError) Error() string {
+	return err.Message
 }
 
 //NewHttpClient Construct a new HttpClient
@@ -31,14 +45,27 @@ func checkResponseStatus(resp *http.Response, expected int) error {
 	if resp.StatusCode != expected {
 		body, err := ioutil.ReadAll(resp.Body)
 		if err != nil {
-			return fmt.Errorf("unexpected error attempting to read error response body: %s", err.Error())
+			return &APIError{
+				ActualStatus:   resp.StatusCode,
+				ExpectedStatus: expected,
+				Message:        fmt.Sprintf(readResponseBodyErrFmt, err.Error(), req.Method, req.URL.RequestURI(), expected, resp.StatusCode),
+			}
 		}
 
 		if len(body) > 0 {
-			return fmt.Errorf(incorrectStatusWithBodyErrFmt, req.Method, req.URL.RequestURI(), expected, resp.StatusCode, string(body))
+			return &APIError{
+				ActualStatus:   resp.StatusCode,
+				ExpectedStatus: expected,
+				Body:           string(body),
+				Message:        fmt.Sprintf(incorrectStatusWithBodyErrFmt, req.Method, req.URL.RequestURI(), expected, resp.StatusCode),
+			}
 		}
 
-		return fmt.Errorf(incorrectStatusErrFmt, req.Method, req.URL.RequestURI(), expected, resp.StatusCode)
+		return &APIError{
+			ActualStatus:   resp.StatusCode,
+			ExpectedStatus: expected,
+			Message:        fmt.Sprintf(incorrectStatusErrFmt, req.Method, req.URL.RequestURI(), expected, resp.StatusCode),
+		}
 	}
 	return nil
 }
