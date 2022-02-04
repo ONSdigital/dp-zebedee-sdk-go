4648b7e5c107cbf0c201138ae44cdfb5d89ac16c
Dave Llewellyn <439837@gmail.com>
2022-02-04 15:07:40 +0000

    updated script

--- ci/scripts/doGoLint.sh
+++ ci/scripts/doGoLint.sh
@@ -1,13 +1,7 @@
 #!/bin/bash -eux
 
+echo "installing Go lint..."
 go get golang.org/x/lint
-
-if [ "$(golint ./... | wc -l)" -gt 0 ]
-  then
-    golint ./...
-    echo "Go linter returned errors"
-    exit 1
-fi
 
 if [ "$(golint ./... | wc -l)" -gt 0 ]
   then
