58bde0eaaf8a6024661c3bad2488e74c45b536cd
s-xton <stephen.saxton@ons.gov.uk>
2023-07-04 10:57:12 +0100

    added content api

---
+++ zebedee/content.go
@@ -0,0 +1,32 @@
+package zebedee
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func (z *zebedeeClient) GetContent(s Session, collectionName string, path string) ([]byte, error) {
+	uri := fmt.Sprintf("/Content/%s?uri=%s", collectionName, path)
+	req, err := z.newAuthenticatedRequest(uri, s.ID, http.MethodGet, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := z.do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if err = checkResponseStatus(resp, 200); err != nil {
+		return nil, err
+	}
+
+	b, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return b, nil
+}
